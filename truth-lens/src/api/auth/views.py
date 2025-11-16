dac8a0ff080482c46c201fbc26f53df95f88d609
Aliyan167 <aleeyan33333@gmail.com>
2025-11-16 11:08:20 +0500

    apis created

--- truth-lens/src/api/auth/views.py
+++ truth-lens/src/api/auth/views.py
@@ -1,4 +1,3 @@
-from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.apple.views import AppleOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from dj_rest_auth.registration.views import SocialLoginView, SocialConnectView
