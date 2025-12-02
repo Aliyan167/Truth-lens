2690b3200e72333e7c9a1cd10366df17fbd594cf
Aliyan Ali <aleeyan33333@gmail.com>
2025-12-02 17:46:44 +0500

    Delete src directory

--- src/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-
-urlpatterns = [
-    path('', include('src.website.urls')),
-    path('projects/', include('src.project.urls')),
-    path('services/', include('src.service.urls')),
-    path('team/', include('src.team.urls')),
-]
