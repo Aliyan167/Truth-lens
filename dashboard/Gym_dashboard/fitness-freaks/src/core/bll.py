ff7e8ba13f4331035708f58c1874f8493dca1db8
Aliyan Ali <aleeyan33333@gmail.com>
2026-01-15 01:31:05 +0500

    Delete dashboard directory

--- dashboard/Gym_dashboard/fitness-freaks/src/core/bll.py
+++
@@ -1,6 +0,0 @@
-from .models import Application
-
-
-def get_or_create_application():
-    applications = Application.objects.all()
-    return applications[0] if applications else Application.objects.create()
